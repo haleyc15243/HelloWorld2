1a5032a3c7eb4f7a9295260431cf03489f6fea4e
Haley Cunningham <richard.cunningham@maine.edu>
2019-01-30 08:05:41 -0800

    feature1

--- eclipse-workspace/Hello-World/src/HelloWorld.java
+++ eclipse-workspace/Hello-World/src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	//Print "Hello World!"
 	public static void main(String[] args) {
+		System.out.println("Here ye: important announcement...");
 		System.out.println("Hello World!");
+		System.out.println("Goodbye!");
 	}
 }
