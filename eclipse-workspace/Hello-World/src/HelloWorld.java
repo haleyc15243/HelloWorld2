cea032587d61909d0fdc20652dc41d415532206c
Haley Cunningham <richard.cunningham@maine.edu>
2019-01-31 13:49:35 -0800

    HiCharlie

--- eclipse-workspace/Hello-World/src/HelloWorld.java
+++ eclipse-workspace/Hello-World/src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hi Alice");
 		System.out.println("Hi Bob.");
+		System.out.println("Hi Charlie");
 		System.out.println("Goodbye!");
 	}
 }
