168c48783b3bfba9a8e45b89955ddc44ab03cee8
Haley Cunningham <richard.cunningham@maine.edu>
2019-02-12 18:22:53 -0800

    date added

--- eclipse-workspace/Hello-World/src/HelloWorld.java
+++ eclipse-workspace/Hello-World/src/HelloWorld.java
@@ -1,7 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HelloWorld {
 	//Print "Hello World!"
 	public static void main(String[] args) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyy-MM-dd 'at' HH:mm:ss z");
+		Date date = new Date(System.currentTimeMillis());
+		System.out.println("Date: " + formatter.format(date));
 		System.out.println("Here ye: important announcement...");
 		System.out.println("Hello World!");
 		System.out.println("Hi Alice");
